8fa6bfcea53dd037a7c1ba7ef6510f79c7d5288c
Nadman Faisal <nadmanfaisal@gmail.com>
2025-11-17 23:08:26 +0100

    Finish encode and decode

--- encode_and_decode/solution.py
+++ encode_and_decode/solution.py
@@ -1,20 +1,25 @@
 def encode(strs: list[str]) -> str:
     res = ""
-    for str in strs:
-        res += str + "#"
+    for s in strs:
+        res += str(len(s)) + "#" + s
     return res
 
 
 def decode(s: str) -> list[str]:
-    output = []
-    word = ""
-    for i in range(len(s)):
-        if s[i] == '#':
-            output.append(word)
-            word = ""
-        else:
-            word += s[i]
-    return output
+    res = []
+    i = 0
+
+    while i < len(s):
+        j = i
+        while s[j] != '#':
+            j += 1
+        length = int(s[i:j])
+        i = j + 1
+        j = i + length
+        res.append(s[i:j])
+        i = j
+
+    return res
 
 
 
