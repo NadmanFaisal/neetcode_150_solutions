552681aae976ef7cef86a2bb64ac1bca45b0d4af
Nadman Faisal <nadmanfaisal@gmail.com>
2025-11-26 19:11:57 +0100

    Finish binary tree level order traversal

---
+++ binary_tree_level_order_traversal/solution.py
@@ -0,0 +1,56 @@
+import collections
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
+            
+def build_tree(values: List[Optional[int]]) -> Optional[TreeNode]:
+    if not values:
+        return None
+    
+    root = TreeNode(values[0])
+    queue = collections.deque([root])
+    i = 1
+    
+    while i < len(values):
+        current = queue.popleft()
+        
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+        
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+        
+    return root
+
+if __name__ == "__main__":
+    input_list = [1,2,3,4,5,6,7]
+    root = build_tree(input_list)
+    sol = Solution()
+    print(sol.levelOrder(root))
