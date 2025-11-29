4a9098292cd101a85ebe3aedb09498c082961150
Nadman Faisal <nadmanfaisal@gmail.com>
2025-11-29 12:06:37 +0100

    Finish 3 sum

---
+++ 3_sum/solution.py
@@ -0,0 +1,27 @@
+def threeSum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    output = []
+    
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            sum = a + nums[l] + nums[r]
+            if sum > 0:
+                r -= 1
+            elif sum < 0:
+                l += 1
+            else:
+                output.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return output
+
+
+
+#nums = [-1,0,1,2,-1,-4]
+nums = [-2,0,1,1,2]
+print(threeSum(nums))
